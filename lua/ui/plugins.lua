f38b0d6764bf7172082c8b887817035a70a79c58
Liam <geffc1454@gmail.com>
2025-01-22 12:27:28 +0800

    fix: copilot nvim download fail

--- lua/ui/plugins.lua
+++ lua/ui/plugins.lua
@@ -124,6 +124,7 @@ return {
 			{ "zbirenbaum/copilot.lua" },
 			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
 		},
+		branch = "main",
 		build = "make tiktoken", -- Only on MacOS or Linux
 		opts = {},
 		config = require("ui.copilot-chat"),
